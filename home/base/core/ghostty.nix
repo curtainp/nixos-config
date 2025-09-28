0338fc198cf63541054a8ccf6e16f37b1fe38b17
curtain <curtainwk@gmail.com>
2025-09-29 01:08:50 +0800

    tweak(ghostty): add chinese fonts

--- home/base/core/ghostty.nix
+++ home/base/core/ghostty.nix
@@ -6,6 +6,7 @@
     enable = true;
     settings = {
       font-family = "Iosevka Nerd Font";
+      font-codepoint-map = "U+4E00-U+9FFF,U+3400-U+4DBF = LXGW WenKai Screen";
       font-size = 15;
       macos-option-as-alt = true;
       cursor-style = "block";
