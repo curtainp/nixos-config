99fc3c8e1e6bd27966f85f3f9b6e76788627e863
curtain <curtainwk@gmail.com>
2025-09-28 16:18:57 +0800

    feat(ghostty): add ghostty terminal emulator

---
+++ home/base/core/ghostty.nix
@@ -0,0 +1,18 @@
+{
+  ...
+}:
+{
+  programs.ghostty = {
+    enable = true;
+    settings = {
+      font-family = "Iosevka Nerd Font";
+      font-size = 15;
+      macos-option-as-alt = true;
+      cursor-style = "block";
+      cursor-style-blink = false;
+      window-decoration = "server";
+      fullscreen = false;
+      macos-non-native-fullscreen = "visible-menu";
+    };
+  };
+}
